ce9141027968e541d98bb43e128c41152aac30e1
Carlie Duperval <ritchellcarlie@gmail.com>
2022-06-26 19:57:29 -0400

    Added ComponentHome, Login SignUp

---
+++ src/scenes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Welcome from '../Components/Welcome'
+import Todolist from '../Components/TodoList'
+
+const Home = () => {
+    return(
+        <>
+        <Welcome />
+        <Todolist />
+        </>
+    )
+}
