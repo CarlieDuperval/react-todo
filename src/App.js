4dffedefc15c803540488eb4a10dbc77a8d9ac02
Carlie Duperval <ritchellcarlie@gmail.com>
2022-06-26 20:11:01 -0400

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import firebase from 'firebase'
+import Home from './scenes/Home'
+import Login from './scenes/Login'
+import Signup from './scenes/Signup'
+import TopMenu from './components/TopMenu'
+import { firebaseConfig } from './fbConfig'
+import 'antd/dist/antd.css'
 
-function App() {
+
+
+
+
+
+
+
+
+firebase.initializeApp(firebaseConfig)
+const fbaseAuth = firebase.auth()
+
+export const UserContext = createContext(null)
+const App = ()=> {
+  const [user, setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value={{ user, setUser, fbaseAuth }}>
+      <Router>
+        <TopMenu />
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  )
 }
 
 export default App;
